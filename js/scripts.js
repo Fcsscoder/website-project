8e911a5f73b1d254e8faaa90a44b6e2d24f10627
Fcsscoder <marketing@letscode.com.br>
2024-01-24 18:35:09 -0300

    Firts commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+const myButton = document.querySelector("#btn")
+const myTitle = document.querySelector("#message-title")
+const form = document.querySelector(".form-container")
+const inputName = document.querySelector("#name")
+
+function alterarTexto(nome){
+    if(nome){
+        const text = `Obrigado pela mensagem, ${nome}!`
+        myTitle.textContent = (text)} else {
+        const text = `Obrigado pela mensagem!`
+        myTitle.textContent = (text)
+    }
+}
+
+myButton.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const nome = inputName.value
+
+    alterarTexto(nome)
+
+    if(form){
+    form.remove()
+    }
+})
